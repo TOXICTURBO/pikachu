a3315596313d76128957390cd39b813cad89bc4a
TOXICTURBO <97358745+TOXICTURBO@users.noreply.github.com>
2022-01-27 17:06:36 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -12,7 +12,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Turbo*
 *Creator number : wa.me/916380260672?text=Hi%20Turbo%20Mods.%20*
 
-*Githublink (Setup)  :    coming soon*
+*Githublink :    coming soon*
 
 *Audio commads :   coming soon*
 
